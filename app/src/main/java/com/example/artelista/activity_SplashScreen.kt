a0bc78ef2d24a7110ce6290ad34ab7ab45ef1e37
DJosuep <josuep.garciar@gmail.com>
2023-03-29 10:15:18 -0600

    Finished Practice1
    The end.

--- app/src/main/java/com/example/artelista/activity_SplashScreen.kt
+++ app/src/main/java/com/example/artelista/activity_SplashScreen.kt
@@ -1,11 +1,37 @@
 package com.example.artelista
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class activity_SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        try
+        {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_splash_screen)
+//Agregamos la referencia al ImageView
+            val LogoApp: ImageView = findViewById(R.id.imgArteLista)
+            val AnimLogo: Animation = AnimationUtils.loadAnimation(this, R.anim.anim1)
+            LogoApp.startAnimation(AnimLogo)
+//Intent para crear instancia de la activity Login
+            val intent = Intent(this, activity_Login::class.java)
+            AnimLogo.setAnimationListener(object:
+                Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                }
+                override fun onAnimationEnd(animation: Animation?) {
+                    startActivity(intent)
+                    finish()
+                }
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+            })
+        }
+        catch (e:java.lang.Exception)
+        { e.printStackTrace() }
     }
 }
