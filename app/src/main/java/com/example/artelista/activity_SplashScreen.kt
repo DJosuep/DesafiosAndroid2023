31006ae0358814dbda0f8844f7f6039e2e7c0865
DJosuep <josuep.garciar@gmail.com>
2023-03-29 09:23:00 -0600

    Terminada SplashScreen y agregado Login

--- app/src/main/java/com/example/artelista/activity_SplashScreen.kt
+++ app/src/main/java/com/example/artelista/activity_SplashScreen.kt
@@ -3,7 +3,7 @@ package com.example.artelista
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashScreen : AppCompatActivity() {
+class activity_SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
