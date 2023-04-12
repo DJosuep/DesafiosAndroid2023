6d6d4df409e87f91d8830e0e9832c0549f03a06f
DarcoProgramador <denangel123@gmail.com>
2023-04-11 22:49:41 -0600

    Añadiendo funcionalidad al boton Login

--- ArteLista/app/src/main/java/com/example/artelista/activity_login.kt
+++ ArteLista/app/src/main/java/com/example/artelista/activity_login.kt
@@ -1,11 +1,24 @@
 package com.example.artelista
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class activity_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Intent para crear instancia de la Main_activity
+        val intent = Intent(this, MainActivity::class.java)
+
+        //Crando una referencia al botton
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            startActivity(intent)
+            finish()
+        }
     }
 }
