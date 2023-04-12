82771836824c25a150733afaa5683eb936a08bd7
DarcoProgramador <denangel123@gmail.com>
2023-04-12 00:43:32 -0600

    Añadiendo toolbar y traduccion en ingles

--- ArteLista/app/src/main/java/com/example/artelista/ActivityCrearCuenta.kt
+++ ArteLista/app/src/main/java/com/example/artelista/ActivityCrearCuenta.kt
@@ -1,13 +1,17 @@
 package com.example.artelista
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class ActivityCrearCuenta : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crearcuenta)
+
+        val toolbar:androidx.appcompat.widget.Toolbar = findViewById(R.id.tbCrearCuenta)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 }
