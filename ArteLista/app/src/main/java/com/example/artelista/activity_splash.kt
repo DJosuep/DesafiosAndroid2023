290f458734f8065d6789572ed1214211cd300ee4
DJosuep <josuep.garciar@gmail.com>
2023-04-11 21:30:33 -0600

    Añadidos activities splash y login

---
+++ ArteLista/app/src/main/java/com/example/artelista/activity_splash.kt
@@ -0,0 +1,11 @@
+package com.example.artelista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
