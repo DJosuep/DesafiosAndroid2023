92730e06778bf830f85c2caca54ae6bb17459941
DJosuep <josuep.garciar@gmail.com>
2023-04-11 21:41:33 -0600

    Corregido bug animación

--- ArteLista/app/src/main/java/com/example/artelista/activity_splash.kt
+++ ArteLista/app/src/main/java/com/example/artelista/activity_splash.kt
@@ -1,11 +1,39 @@
 package com.example.artelista
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class activity_splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        try
+        {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_splash)
+
+            //Agregamos la referencia al ImageView
+            val LogoApp: ImageView = findViewById(R.id.imgArteLista)
+            val AnimLogo: Animation = AnimationUtils.loadAnimation(this, R.anim.anim1)
+            LogoApp.startAnimation(AnimLogo)
+
+            //Intent para crear instancia de la activity Login
+            val intent = Intent(this, activity_login::class.java)
+            AnimLogo.setAnimationListener(object:
+                Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                }
+                override fun onAnimationEnd(animation: Animation?) {
+                    startActivity(intent)
+                    finish()
+                }
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+            })
+        }
+        catch (e:java.lang.Exception)
+        { e.printStackTrace() }
     }
 }
