45e6ddb8b77e6843465c6be9a5b47cdf20c085da
DarcoProgramador <denangel123@gmail.com>
2023-04-15 10:48:21 -0600

    Añadiendo binding a SplashActivity

--- ArteLista/app/src/main/java/com/example/artelista/SplashActivity.kt
+++ ArteLista/app/src/main/java/com/example/artelista/SplashActivity.kt
@@ -3,21 +3,25 @@ package com.example.artelista
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import com.example.artelista.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         try
         {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_splash)
+            binding = ActivitySplashBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
             //Agregamos la referencia al ImageView
-            val LogoApp: ImageView = findViewById(R.id.imgArteLista)
             val AnimLogo: Animation = AnimationUtils.loadAnimation(this, R.anim.anim1)
-            LogoApp.startAnimation(AnimLogo)
+            binding.imgArteLista.startAnimation(AnimLogo)
 
             //Intent para crear instancia de la activity Login
             val intent = Intent(this, LoginActivity::class.java)
