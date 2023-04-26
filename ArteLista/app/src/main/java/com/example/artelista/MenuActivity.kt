35b932e6b9e4282fb508cb20940bc8dc0320c958
DarcoProgramador <denangel123@gmail.com>
2023-04-25 18:16:21 -0600

    solucionando problema con bottom navbar

--- ArteLista/app/src/main/java/com/example/artelista/MenuActivity.kt
+++ ArteLista/app/src/main/java/com/example/artelista/MenuActivity.kt
@@ -3,7 +3,10 @@ package com.example.artelista
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -20,16 +23,9 @@ class MenuActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_menu) as NavHostFragment
+        val navController = navHostFragment.findNavController()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_menu)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_shopping_cart, R.id.navigation_notifications, R.id.navigation_user
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
