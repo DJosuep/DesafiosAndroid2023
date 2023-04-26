9e97b4305e76d711905672586ceb8fdd1fe7914d
DarcoProgramador <denangel123@gmail.com>
2023-04-25 21:05:27 -0600

    add adapter and model to Notifications

---
+++ ArteLista/app/src/main/java/com/example/artelista/adapter/AdapterNotifications.kt
@@ -0,0 +1,75 @@
+package com.example.artelista.adapter
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.artelista.R
+import com.example.artelista.model.Notification
+
+class AdapterNotifications(Notifications: ArrayList<Notification>,
+                           resource:Int, activity: Activity): RecyclerView.Adapter<AdapterNotifications.NotificationViewHolder>() {
+    //------------
+    private val notifications: ArrayList<Notification>
+    private val resource: Int
+    private val activity: Activity
+    //------------
+    init {
+        this.notifications = Notifications
+        this.resource = resource
+        this.activity = activity
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(resource, parent, false)
+        return NotificationViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+        val notification : Notification = notifications[position]
+        holder.tvTituloNotification.text = notification.getTituloNotification()
+        holder.tvCategoriaNotification.text = notification.getCategoriaNotification()
+        holder.tvHoraNotification.text = notification.getHoraNotification()
+    }
+    //-----------
+    override fun getItemCount(): Int {
+        return notifications.size
+    }
+    //Adapter versus la interface de cada item
+    inner class NotificationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //---------------
+        val tvTituloNotification: TextView
+        val tvCategoriaNotification: TextView
+        val tvHoraNotification: TextView
+        //---------------
+        init {
+            tvTituloNotification = itemView.findViewById<View>(R.id.tvTituloNotification) as TextView
+            tvCategoriaNotification = itemView.findViewById<View>(R.id.tvCategoriaNotification) as TextView
+            tvHoraNotification = itemView.findViewById<View>(R.id.tvNotificationHora) as TextView
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ArteLista/app/src/main/java/com/example/artelista/model/Notification.kt
@@ -0,0 +1,10 @@
+package com.example.artelista.model
+
+class Notification(private var tituloNotification:String,
+                   private var categoriaNotification:String,
+                   private var horaNotification:String) {
+    fun getTituloNotification():String{return this.tituloNotification}
+    fun getCategoriaNotification():String{return this.categoriaNotification}
+    fun getHoraNotification():String{return this.horaNotification}
+
+}
