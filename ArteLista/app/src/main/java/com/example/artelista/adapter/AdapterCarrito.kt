754677fdf6936185e1ea5cf647fe2f4640ccf46c
DJosuep <josuep.garciar@gmail.com>
2023-04-25 20:57:07 -0600

    Añadidos paquetes model y adapter

---
+++ ArteLista/app/src/main/java/com/example/artelista/adapter/AdapterCarrito.kt
@@ -0,0 +1,4 @@
+package com.example.artelista.adapter
+
+class AdapterCarrito {
+}
---
+++ ArteLista/app/src/main/java/com/example/artelista/model/Carrito.kt
@@ -0,0 +1,13 @@
+package com.example.artelista.model
+
+class Carrito(
+    private var artistaCarrito:String,
+    private var precioCarrito:String,
+    private var imagenCarrito:String, 
+    private var tituloCarrito:String
+    ) {
+    fun getArtistaCarrito():String{ return this.artistaCarrito }
+    fun getPrecioCarrito():String{ return this.precioCarrito }
+    fun getImagenCarrito():String{ return this.imagenCarrito }
+    fun getTituloCarrito():String{ return this.tituloCarrito }
+}
