f52509c30b3bf8a1c85de8f41dac703e33d614c1
DJosuep <josuep.garciar@gmail.com>
2023-04-25 19:45:33 -0600

    Solucionados errores faltantes de rebase

--- ArteLista/app/src/main/java/com/example/artelista/MenuActivity.kt
+++ ArteLista/app/src/main/java/com/example/artelista/MenuActivity.kt
@@ -3,7 +3,6 @@ package com.example.artelista
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -33,15 +32,15 @@ class MenuActivity : AppCompatActivity() {
 
         val view = binding.root
         setContentView(view)
-        configurarNavegacion()
+        configurarNavegacion(navHostFragment)
 
         //Color top action bar
         //Objects.requireNonNull(supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.WHITE)))
     }
 
-    protected fun configurarNavegacion(){
+    protected fun configurarNavegacion(navHostFragment:NavHostFragment){
         val bmenu: BottomNavigationView = binding.navView
-        NavigationUI.setupWithNavController(bmenu, Navigation.findNavController(this, R.id.nav_host_fragment_activity_menu))
+        NavigationUI.setupWithNavController(bmenu, navHostFragment.navController)
     }
 
     private fun setContentView(HomeFragment: Any) {
