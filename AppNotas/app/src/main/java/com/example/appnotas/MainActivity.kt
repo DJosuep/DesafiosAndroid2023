0a16e44208d799864d9dec33859005e6805a8be4
DarcoProgramador <denangel123@gmail.com>
2023-05-03 10:32:22 -0600

    Añadiendo el binding a MainActivity

--- AppNotas/app/src/main/java/com/example/appnotas/MainActivity.kt
+++ AppNotas/app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.appnotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appnotas.adapter.AdapterAnotacion
 import com.example.appnotas.databinding.ActivityMainBinding
 import com.example.appnotas.model.Anotacion
 
 class MainActivity : AppCompatActivity(), OnClickListener {
+    //-----------------
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var anotacionAdapter: AdapterAnotacion
+    //-----------------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
